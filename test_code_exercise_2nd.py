2b1bedcda2f002557421e806b67dc2d04c3e0763
Skaur246 <skaur246@rrc.ca>
2025-10-04 23:07:20 -0500

    my first commit for 2nd exercise

---
+++ test_code_exercise_2nd.py
@@ -0,0 +1,29 @@
+
+
+"""
+
+# Check for odd or even numbers in list 1
+numbers1 = [10, 15, 22, 29, 30]
+for num in numbers1:
+    if num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} is odd.")
+
+# Check for odd or even numbers in list 2
+numbers2 = [1, 4, 7, 9, 12]
+for num in numbers2:
+    if num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} is odd.")
+
+# Check for odd or even numbers in list 3
+numbers3 = [100, 150, 200]
+for num in numbers3:
+    if num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} is odd.")
+"""
+
