b8c4849efc3e7957827d6c2bcfa4648b7dcd4df5
Zainab Hassan <zainabmosunmola@gmail.com>
2025-10-04 21:40:22 -0500

    refactored area of rectangle calculation

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from output import area_of_shapes
 from area_calculation import area_of_circle
+from area_calculation import area_of_rectangle
 
 # Calculating area of rectangles
-length1 = 10
-width1 = 5
-area_rectangle1 = length1 * width1
-
-length2 = 15
-width2 = 7
-area_rectangle2 = length2 * width2
+rectangle_parameter = [(10, 5), (15, 7)]
+area_rectangle_list = area_of_rectangle(rectangle_parameter)
 
 # Calculating area of circles
 radius = [4, 6]
 area_circle_list = area_of_circle(radius)
 
 # printing result
-area_of_shapes(area_rectangle1, area_rectangle2, area_circle_list[0], area_circle_list[1])
+area_of_shapes(area_rectangle_list[0], area_rectangle_list[1], area_circle_list[0], area_circle_list[1])
