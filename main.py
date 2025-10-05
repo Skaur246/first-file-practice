a685a8b73ea6ff161a840cb314d1deb6adbcc564
Zainab Hassan <zainabmosunmola@gmail.com>
2025-10-04 20:27:03 -0500

    refactoring the output

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-# # Calculating area of rectangles
-# length1 = 10
-# width1 = 5
-# area_rectangle1 = length1 * width1
+from output import area_of_shapes
 
-# length2 = 15
-# width2 = 7
-# area_rectangle2 = length2 * width2
+# Calculating area of rectangles
+length1 = 10
+width1 = 5
+area_rectangle1 = length1 * width1
 
-# # Calculating area of circles
-# radius1 = 4
-# area_circle1 = 3.14 * (radius1 ** 2)
+length2 = 15
+width2 = 7
+area_rectangle2 = length2 * width2
 
-# radius2 = 6
-# area_circle2 = 3.14 * (radius2 ** 2)
+# Calculating area of circles
+radius1 = 4
+area_circle1 = 3.14 * (radius1 ** 2)
 
-# print("Area of first rectangle:", area_rectangle1)
-# print("Area of second rectangle:", area_rectangle2)
-# print("Area of first circle:", area_circle1)
-# print("Area of second circle:", area_circle2)
+radius2 = 6
+area_circle2 = 3.14 * (radius2 ** 2)
+
+area_of_shapes(area_rectangle1, area_rectangle2, area_circle1, area_circle2)
---
+++ output.py
@@ -0,0 +1,5 @@
+def area_of_shapes(area_rectangle1, area_rectangle2, area_circle1, area_circle2):
+    print("Area of first rectangle:", area_rectangle1)
+    print("Area of second rectangle:", area_rectangle2)
+    print("Area of first circle:", area_circle1)
+    print("Area of second circle:", area_circle2)
